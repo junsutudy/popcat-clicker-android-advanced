de7d6c06d5d053144da7799d08ee5d6f484d6063
junsuPark <krpjs0508@gmail.com>
2023-03-20 10:56:05 +0900

    nutshell

--- app/src/main/java/app/junsu/popcat/MainActivity.java
+++ app/src/main/java/app/junsu/popcat/MainActivity.java
@@ -1,6 +1,7 @@
 package app.junsu.popcat;
 
 import android.annotation.SuppressLint;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.widget.ImageView;
@@ -15,11 +16,17 @@ public class MainActivity extends AppCompatActivity {
 
     Long clickCount = 0L;
 
+    SoundPool popSound;
+    int popSoundId;
+
     @SuppressLint({"ClickableViewAccessibility", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        popSound = new SoundPool.Builder().build();
+        popSoundId = popSound.load(this, R.raw.raw_pop, 1);
 
         imgMainPopCat = findViewById(R.id.img_main_popcat);
         tvMainCounter = findViewById(R.id.tv_main_counter);
@@ -31,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case (MotionEvent.ACTION_DOWN):
                     this.imgMainPopCat.setImageResource(R.drawable.img_popcat_popped);
+                    popSound.play(popSoundId, 1f, 1f, 0, 0, 1f);
                     clickCount++;
                     tvMainCounter.setText(clickCount.toString());
                     return true;
